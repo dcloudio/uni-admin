3bb2ed790a9517c0a975893b85ce23f7dd79f34e
fxy060608 <fxy060608@gmail.com>
2020-09-25 21:06:17 +0800

    feat: add postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+module.exports = {
+    parser: 'postcss-comment',
+    plugins: {
+        'postcss-import': {
+            resolve(id, basedir, importOptions) {
+                if (id.startsWith('~@/')) {
+                    return path.resolve(process.env.UNI_INPUT_DIR, id.substr(3))
+                } else if (id.startsWith('@/')) {
+                    return path.resolve(process.env.UNI_INPUT_DIR, id.substr(2))
+                } else if (id.startsWith('/') && !id.startsWith('//')) {
+                    return path.resolve(process.env.UNI_INPUT_DIR, id.substr(1))
+                }
+                return id
+            }
+        },
+        'autoprefixer': {
+            overrideBrowserslist: ["> 1%", "last 2 versions", "not dead"],
+            remove: process.env.UNI_PLATFORM !== 'h5'
+        },
+        '@dcloudio/vue-cli-plugin-uni/packages/postcss': {}
+    }
+}
