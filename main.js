05b3bd4e4c8256b30efb90bedb2f81a1b9418ff7
fxy060608 <fxy060608@gmail.com>
2020-09-27 17:12:53 +0800

    feat: add plugin

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store'
+import plugin from './plugin'
 
 Vue.config.productionTip = false
+
+Vue.use(plugin)
 
 App.mpType = 'app'
 
---
+++ plugin.js
@@ -0,0 +1,12 @@
+export default {
+    install(Vue) {
+        Vue.prototype.$hasPermission = function hasPermission(name) {
+            // TODO
+        }
+        uni.addInterceptor('navigateTo', {
+            invoke(params) {
+                // TODO
+            }
+        })
+    }
+}
