b2ea524eaf83af7c3a418f27a86adc8ae48f9e76
fxy060608 <fxy060608@gmail.com>
2020-10-12 20:33:08 +0800

    refactor: service/base/storage

--- cloudfunctions-aliyun/uni-admin/controller/base/storage.js
+++ cloudfunctions-aliyun/uni-admin/controller/base/storage.js
@@ -3,14 +3,14 @@ const {
 } = require('uni-cloud-router')
 module.exports = class StorageController extends Controller {
     async report() {
-        return this.service.storage.report(this.ctx.data)
+        return this.service.base.storage.report(this.ctx.data)
     }
 
     async list() {
-        return this.service.storage.list(this.ctx.data)
+        return this.service.base.storage.list(this.ctx.data)
     }
 
     async delete() {
-        return this.service.storage.delete(this.ctx.data.fileID)
+        return this.service.base.storage.delete(this.ctx.data.fileID)
     }
 }
