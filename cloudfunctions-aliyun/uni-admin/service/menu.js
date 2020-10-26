340af71d988f4f499906877602aa5980e678058b
wangyaqi <wangyaqi988@163.com>
2020-10-26 11:43:35 +0800

    chore: menu collection add menu_id

--- cloudfunctions-aliyun/uni-admin/service/menu.js
+++ cloudfunctions-aliyun/uni-admin/service/menu.js
@@ -3,18 +3,9 @@ const {
 } = require('uni-cloud-router')
 
 function buildMenus(menuList) {
-    // 简单实现删除无subMenu且无url的菜单项
-    // let lastMenuList = []
-    // while (lastMenuList.length !== menuList.length) {
-    //     lastMenuList = menuList
-    //     menuList = menuList.filter(item => {
-    //         const subMenu = menuList.filter(subMenuItem => subMenuItem.parent_id === item._id)
-    //         return item.url || subMenu.length
-    //     })
-    // }
     // 保证父子级顺序
     menuList = menuList.sort(function(a, b) {
-        if (a.parent_id === b._id) {
+        if (a.parent_id === b.menu_id) {
             return 1
         }
         return -1
@@ -22,7 +13,7 @@ function buildMenus(menuList) {
     // 删除无subMenu且无url的菜单项
     for (let i = menuList.length - 1; i > -1; i--) {
         const currentMenu = menuList[i]
-        const subMenu = menuList.filter(subMenuItem => subMenuItem.parent_id === currentMenu._id)
+        const subMenu = menuList.filter(subMenuItem => subMenuItem.parent_id === currentMenu.menu_id)
         if (!currentMenu.url && !subMenu.length) {
             menuList.splice(i, 1)
         }
@@ -36,7 +27,7 @@ function buildMenus(menuList) {
             if (currentMenu.url) {
                 continue
             }
-            const subMenu = menuList.filter(item => item.parent_id === currentMenu._id)
+            const subMenu = menuList.filter(item => item.parent_id === currentMenu.menu_id)
             nextLayer = nextLayer.concat(subMenu)
             currentMenu.children = subMenu
         }
@@ -54,7 +45,7 @@ module.exports = class MenuService extends Service {
         let {
             data: menuList
         } = await this.db.collection('opendb-admin-menus').where({
-            status: 1
+            enable: true
         }).orderBy('sort', 'asc').limit(1000).get()
 
         // 删除无权限访问的菜单
