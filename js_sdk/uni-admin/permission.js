eac85a31043cc63bcc34a26e6381ba41cc41efec
fxy060608 <fxy060608@gmail.com>
2020-11-04 15:03:12 +0800

    fix: permission

--- js_sdk/uni-admin/permission.js
+++ js_sdk/uni-admin/permission.js
@@ -1,11 +1,10 @@
-import store from '@/store'
 export function initPermission(Vue) {
-    Vue.prototype.$hasPermission = function hasPermission(name) {
-        const permission = store.getters['user/userInfo'].permission || []
-        return permission.indexOf(name) > -1
-    }
-    Vue.prototype.$hasRole = function hasRole(name) {
-        const role = store.getters['user/userInfo'].role || []
-        return role.indexOf(name) > -1
-    }
+	Vue.prototype.$hasPermission = function hasPermission(name) {
+		const permission = this.$store.state.user.userInfo.permission || []
+		return permission.indexOf(name) > -1
+	}
+	Vue.prototype.$hasRole = function hasRole(name) {
+		const role = this.$store.state.user.userInfo.role || []
+		return role.indexOf(name) > -1
+	}
 }
