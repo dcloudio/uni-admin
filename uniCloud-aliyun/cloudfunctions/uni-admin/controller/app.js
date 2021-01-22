76657849721d8dd48c17c9920f34f09015c45c3f
wangyaqi <wangyaqi988@163.com>
2021-01-22 19:59:16 +0800

    Revert "build(deps): bump uni-id from 2.0.3 to 3.0.0"
    This reverts commit 5ea0c0bb83a5291db05d405fe04355120c56d3f0.

--- uniCloud-aliyun/cloudfunctions/uni-admin/controller/app.js
+++ uniCloud-aliyun/cloudfunctions/uni-admin/controller/app.js
@@ -1,18 +1,16 @@
 const {
-	Controller
+    Controller
 } = require('uni-cloud-router')
 module.exports = class UserController extends Controller {
-	async init() {
-		const getCurrentUserInfoRes = await this.service.user.getCurrentUserInfo(['_id','username'])
-		return {
-			userInfo: {
-				...getCurrentUserInfoRes.userInfo,
-				...this.ctx.auth.userInfo,
-				token: undefined,
-				password: undefined,
-				permission: this.ctx.auth.permission
-			},
-			navMenu: await this.service.menu.getMenu()
-		}
-	}
+    async init() {
+        return {
+            userInfo: {
+                ...this.ctx.auth.userInfo,
+                token: undefined,
+                password: undefined,
+                permission: this.ctx.auth.permission
+            },
+            navMenu: await this.service.menu.getMenu()
+        }
+    }
 }
--- uniCloud-aliyun/cloudfunctions/uni-admin/middleware/auth.js
+++ uniCloud-aliyun/cloudfunctions/uni-admin/middleware/auth.js
@@ -7,9 +7,7 @@ module.exports = (options) => {
     // 返回中间件函数
     return async function auth(ctx, next) {
         // 校验 token
-        const auth = await uniID.checkToken(ctx.event.uniIdToken, {
-			needPermission: true
-		})
+        const auth = await uniID.checkToken(ctx.event.uniIdToken)
         if (auth.code) {
             // 校验失败，抛出错误信息
             ctx.throw('TOKEN_INVALID', `${auth.message}，${auth.code}`)
