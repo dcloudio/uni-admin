daf2955f726b168d1e0ae029a11de01b65a60fa9
DCloud_LXH <283700113@qq.com>
2021-03-11 20:39:14 +0800

    fix: 1. 修复.uni-group类无法居中 2. 修复 request 请求token无效时返回登陆界面 3. 更新组件 uni-forms 4. 组件 uni-data-checkbox 添加全局禁用属性 5. 组件table的uni-tr组件添加checkedable属性，可以决定该行是否可多选

--- js_sdk/uni-admin/request.js
+++ js_sdk/uni-admin/request.js
@@ -13,6 +13,13 @@ db.on('refreshToken', function({
 	})
 })
 
+db.on('error', function({
+	code, // 错误码详见https://uniapp.dcloud.net.cn/uniCloud/clientdb?id=returnvalue
+	message
+}) {
+	reLaunchToLogin(code)
+})
+
 export function request(action, data, {
 	functionName = 'uni-admin',
 	showModal = true
@@ -30,11 +37,7 @@ export function request(action, data, {
 			return Promise.resolve(result)
 		}
 		if (result.code) {
-			if (typeof result.code === 'string' && result.code.indexOf('TOKEN_INVALID') === 0) {
-				uni.reLaunch({
-					url: config.login.url
-				})
-			}
+			reLaunchToLogin(result.code)
 			// const err = new Error(result.message)
 			// err.code = result.code
 			const err = result
@@ -83,6 +86,14 @@ export function request(action, data, {
 		// #endif
 		return Promise.reject(err)
 	})
+}
+
+function reLaunchToLogin(code) {
+	if (typeof code === 'string' && code.indexOf('TOKEN_INVALID') === 0) {
+		uni.reLaunch({
+			url: config.login.url
+		})
+	}
 }
 
 export function initRequest(Vue) {
