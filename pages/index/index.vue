5e06a3d7ac1e9a47b980d148d6c2283d570c4fde
tianjiaxing <tianjiaxing2016@163.com>
2021-03-17 18:19:30 +0800

    opti: product text for user, publish v1.3.5

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
     <scroll-view style="padding: 15px;box-sizing: border-box;">
-        内容主体，可自定义内容及样式<text style="color: #666; font-size: 15px;">（ uniCloud admin 当前版本号 {{adminVersion}}, 运行到浏览器可在控制台打印信息中查看当前的版本，也可在代码 package.json 中查看）</text>
+        内容主体，可自定义内容及样式<text style="color: #666; font-size: 15px;">（ uniCloud admin {{adminVersion}}, 可在控制台打印信息和 package.json 中查看当前版本号）</text>
 		<!-- #ifndef H5 -->
 		<fix-window />
 		<!-- #endif -->
