fc5f43dc998dc64507f052c5713911ef3061eb90
tianjiaxing <tianjiaxing2016@163.com>
2021-04-16 18:15:59 +0800

    update uni-table

--- uni_modules/uni-table/components/uni-tr/uni-tr.vue
+++ uni_modules/uni-table/components/uni-tr/uni-tr.vue
@@ -1,136 +1,162 @@
 <template>
+	<!-- #ifdef H5 -->
+	<tr class="uni-table-tr">
+		<th v-if="selection === 'selection' && ishead" class="checkbox" :class="{ 'tr-table--border': border }">
+			<table-checkbox :checked="checked" :indeterminate="indeterminate" :disabled="disabled" @checkboxSelected="checkboxSelected"></table-checkbox>
+		</th>
+		<slot></slot>
+		<!-- <uni-th class="th-fixed">123</uni-th> -->
+	</tr>
+	<!-- #endif -->
+	<!-- #ifndef H5 -->
 	<view class="uni-table-tr">
-		<checkbox-group v-if="selection === 'selection'" class="checkbox" :class="{'tr-table--border':border}" @change="change">
-			<label>
-				<checkbox value="check" :checked="value" :disabled="!selectable" />
-			</label>
-		</checkbox-group>
+		<view v-if="selection === 'selection' " class="checkbox" :class="{ 'tr-table--border': border }">
+			<table-checkbox :checked="checked" :indeterminate="indeterminate" :disabled="disabled" @checkboxSelected="checkboxSelected"></table-checkbox>
+		</view>
 		<slot></slot>
 	</view>
+	<!-- #endif -->
 </template>
 
 <script>
-	/**
-	 * Tr 表格行组件
-	 * @description 表格行组件 仅包含 th,td 组件
-	 * @tutorial https://ext.dcloud.net.cn/plugin?id=
-	 */
-	export default {
-		name: 'uniTr',
-		props: {
-			selectable: {
-				type: Boolean,
-				default: true
-			}
+/**
+ * Tr 表格行组件
+ * @description 表格行组件 仅包含 th,td 组件
+ * @tutorial https://ext.dcloud.net.cn/plugin?id=
+ */
+import tableCheckbox from './table-checkbox.vue'
+export default {
+	name: 'uniTr',
+	components: { tableCheckbox },
+	props: {
+		disabled: {
+			type: Boolean,
+			default: false
 		},
-		options: {
-			virtualHost: true
+		keyValue: {
+			type: [String, Number],
+			default: ''
+		}
+	},
+	options: {
+		virtualHost: true
+	},
+	data() {
+		return {
+			value: false,
+			border: false,
+			selection: false,
+			widthThArr: [],
+			ishead: true,
+			checked: false,
+			indeterminate:false
+		}
+	},
+	created() {
+		this.root = this.getTable()
+		this.head = this.getTable('uniThead')
+		if (this.head) {
+			this.ishead = false
+			this.head.init(this)
+		}
+		this.border = this.root.border
+		this.selection = this.root.type
+		this.root.trChildren.push(this)
+		const rowData = this.root.data.find(v => v[this.root.rowKey] === this.keyValue)
+		if(rowData){
+			this.rowData = rowData
+		}
+		this.root.isNodata()
+	},
+	mounted() {
+		if (this.widthThArr.length > 0) {
+			const selectionWidth = this.selection === 'selection' ? 50 : 0
+			this.root.minWidth = this.widthThArr.reduce((a, b) => Number(a) + Number(b)) + selectionWidth
+		}
+	},
+	destroyed() {
+		const index = this.root.trChildren.findIndex(i => i === this)
+		this.root.trChildren.splice(index, 1)
+		this.root.isNodata()
+	},
+	methods: {
+		minWidthUpdate(width) {
+			this.widthThArr.push(width)
 		},
-		data() {
-			return {
-				value: false,
-				border: false,
-				selection: false,
-				widthThArr: []
-			};
+		// 选中
+		checkboxSelected(e) {
+			let rootData = this.root.data.find(v => v[this.root.rowKey] === this.keyValue)
+			this.checked = e.checked
+			this.root.check(rootData||this, e.checked,rootData? this.keyValue:null)
 		},
-		created() {
-			this.root = this.getTable()
-			this.border = this.root.border
-			this.selection = this.root.type
-			this.root.trChildren.push(this)
-			this.root.isNodata()
-		},
-		mounted() {
-			if (this.widthThArr.length > 0) {
-				const selectionWidth = this.selection === 'selection' ? 50 : 0
-				this.root.minWidth = this.widthThArr.reduce((a, b) => Number(a) + Number(b)) + selectionWidth
-			}
-		},
-		destroyed() {
-			const index = this.root.trChildren.findIndex(i => i === this)
-			this.root.trChildren.splice(index, 1)
-			this.root.isNodata()
-		},
-		methods: {
-			minWidthUpdate(width) {
-				this.widthThArr.push(width)
-			},
-			change(e) {
-				this.root.trChildren.forEach((item) => {
-					if (item === this) {
-						this.root.check(this, e.detail.value.length > 0 ? true : false)
-					}
-				})
-			},
-			/**
-			 * 获取父元素实例
-			 */
-			getTable() {
-				let parent = this.$parent;
-				let parentName = parent.$options.name;
-				while (parentName !== 'uniTable') {
-					parent = parent.$parent;
-					if (!parent) return false;
-					parentName = parent.$options.name;
+		change(e) {
+			this.root.trChildren.forEach(item => {
+				if (item === this) {
+					this.root.check(this, e.detail.value.length > 0 ? true : false)
 				}
-				return parent;
-			},
+			})
+		},
+		/**
+		 * 获取父元素实例
+		 */
+		getTable(name = 'uniTable') {
+			let parent = this.$parent
+			let parentName = parent.$options.name
+			while (parentName !== name) {
+				parent = parent.$parent
+				if (!parent) return false
+				parentName = parent.$options.name
+			}
+			return parent
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-	.uni-table-tr {
-		/* #ifndef APP-NVUE */
-		display: table-row;
-		transition: all .3s;
-		box-sizing: border-box;
-		/* #endif */
-	}
+$border-color: #ebeef5;
 
-	.checkbox {
-		padding: 12px 8px;
-		width: 26px;
-		padding-left: 12px;
-		/* #ifndef APP-NVUE */
-		display: table-cell;
-		vertical-align: middle;
-		/* #endif */
-		color: #333;
-		font-weight: 500;
-		border-bottom: 1px #ddd solid;
-		font-size: 14px;
-		text-align: center;
-	}
-
-	.tr-table--border {
-		border-right: 1px #ddd solid;
-	}
-
-	::v-deep .tr-table--border uni-checkbox .uni-checkbox-input {
-		width: 15px;
-		height: 15px;
-	}
-	
-	::v-deep .tr-table--border uni-checkbox .uni-checkbox-input.uni-checkbox-input-checked:before {
-		font-size: 14px;
-	}
-
+.uni-table-tr {
 	/* #ifndef APP-NVUE */
-	.uni-table-tr {
-		::v-deep .uni-table-th {
-			&.table--border:last-child {
-				border-right: none;
-			}
-		}
+	display: table-row;
+	transition: all 0.3s;
+	box-sizing: border-box;
+	/* #endif */
+}
 
-		::v-deep .uni-table-td {
-			&.table--border:last-child {
-				border-right: none;
-			}
+.checkbox {
+	padding: 0 8px;
+	width: 26px;
+	padding-left: 12px;
+	/* #ifndef APP-NVUE */
+	display: table-cell;
+	vertical-align: middle;
+	/* #endif */
+	color: #333;
+	font-weight: 500;
+	border-bottom: 1px $border-color solid;
+	font-size: 14px;
+	// text-align: center;
+}
+
+.tr-table--border {
+	border-right: 1px $border-color solid;
+}
+
+/* #ifndef APP-NVUE */
+.uni-table-tr {
+	::v-deep .uni-table-th {
+		&.table--border:last-child {
+			// border-right: none;
 		}
 	}
 
-	/* #endif */
+	::v-deep .uni-table-td {
+		&.table--border:last-child {
+			// border-right: none;
+		}
+	}
+}
+
+/* #endif */
 </style>
