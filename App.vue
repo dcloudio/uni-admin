acb3906cb3e7e5a85d0cbce7229c382d3dbb5489
tianjiaxing <tianjiaxing2016@163.com>
2021-11-11 14:32:01 +0800

    chore: console invite job ad

--- App.vue
+++ App.vue
@@ -4,7 +4,9 @@
 		mapActions
 	} from 'vuex'
 	import config from '@/admin.config.js'
-	import { version } from './package.json'
+	import {
+		version
+	} from './package.json'
 	export default {
 		computed: {
 			...mapGetters({
@@ -23,11 +25,12 @@
 		},
 		onLaunch: function() {
 			console.log(
-			        `%c uniCloud Admin %c 当前版本号 v${version} %c`,
-			        'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
-			        'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;',
-			        'background:transparent'
-			      )
+				`%c uniCloud Admin %c 当前版本号 v${version} %c`,
+				'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
+				'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;',
+				'background:transparent'
+			)
+			console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
 			console.log('App Launch')
 			if (!this.isTokenValid) {
 				uni.redirectTo({
