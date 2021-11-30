df377db1efddba19b8973c0f7f4e462c8a93b3c5
tianjiaxing <tianjiaxing2016@163.com>
2021-11-30 16:33:28 +0800

    fix: android change language will crash, temporarily ban it

--- App.vue
+++ App.vue
@@ -24,13 +24,16 @@
 			})
 		},
 		onLaunch: function() {
+			// #ifdef H5
 			console.log(
 				`%c uniCloud Admin %c v${version} %c`,
 				'background:#35495e ; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff',
 				'background:#007aff ;padding: 1px; border-radius: 0 3px 3px 0;  color: #fff; font-weight: bold;',
 				'background:transparent'
 			)
-			console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
+			// #endif
+			// 线上示例使用
+			// console.log('%c uni-app官方团队诚邀优秀前端工程师加盟，一起打造更卓越的uni-app & uniCloud，欢迎投递简历到 hr2013@dcloud.io', 'color: red');
 			console.log('App Launch')
 			if (!this.isTokenValid) {
 				uni.redirectTo({
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import store from './store'
 import plugin from './js_sdk/uni-admin/plugin'
 import messages from './i18n/index.js'
 
+const lang = uni.getLocale()
 // #ifndef VUE3
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
@@ -10,7 +11,7 @@ Vue.config.productionTip = false
 Vue.use(VueI18n)
 // 通过选项创建 VueI18n 实例
 const i18n = new VueI18n({
-  locale: 'zh-Hans', // 设置地区
+  locale: lang, // 设置地区
   messages, // 设置地区信息
 })
 Vue.use(plugin)
@@ -29,7 +30,7 @@ import { createI18n } from 'vue-i18n'
 export function createApp() {
   const app = createSSRApp(App)
   const i18n = createI18n({
-  	locale: 'zh-Hans',
+  	locale: lang,
   	messages
   })
   app.use(i18n)
