6f50240687af19376c5b353a120638875ef22c73
tianjiaxing <tianjiaxing2016@163.com>
2022-01-19 20:05:48 +0800

    wip(stat): comparison done

--- pages/uni-stat/comparison/comparison.vue
+++ pages/uni-stat/comparison/comparison.vue
@@ -17,9 +17,15 @@
 				</view>
 			</view>
 			<view class="flex">
-				<view class="uni-stat--x uni-charts-box">
-					<view class="label-text">日新增用户对比</view>
-					<qiun-data-charts type="ring" :opts="{legend:{position:'left'}}" :chartData="chartData"
+				<view v-for="(item,index) in dayChartsData" class="uni-stat--x uni-charts-box">
+					<view class="label-text">{{dayChartsData[index].title}}</view>
+					<qiun-data-charts type="ring" :opts="{legend:{position:'left'}}" :chartData="dayChartsData[index]"
+						:echartsApp="true" />
+				</view>
+
+				<view v-for="(item,index) in monChartsData" class="uni-stat--x uni-charts-box">
+					<view class="label-text">{{monChartsData[index].title}}</view>
+					<qiun-data-charts type="ring" :opts="{legend:{position:'left'}}" :chartData="monChartsData[index]"
 						:echartsApp="true" />
 				</view>
 			</view>
@@ -48,180 +54,130 @@
 					appid: '',
 					start_time: 1642089600000,
 				},
-				chartData: {}
+				platforms: [],
+				dayChartsData: [],
+				monChartsData: []
 			}
 		},
 		mounted() {
 			this.getChartData(this.query)
-			// this.getRangeCountData(this.query, 'month')
+			this.getRangeCountData(this.query, 'month')
 		},
 		watch: {
 			query: {
 				deep: true,
 				handler(val) {
 					this.getChartData(val)
+					this.getRangeCountData(val, 'month')
 				}
 			}
 		},
 
 		methods: {
-			getChartData(query, type='') {
-				const options = {
-					series: [{
-						data: []
-					}]
-				}
+			getChartData(query, type = 'day') {
 				query = stringifyQuery(query)
 				console.log('.............query:', query);
 				const db = uniCloud.database()
-
-				const main = db.collection('opendb-stat-app-platforms').getTemp()
-				const sub = db.collection('opendb-stat-app-session-result')
+				db.collection('opendb-stat-app-session-result')
 					.where(query)
-					.getTemp()
-
-				db.collection(main, sub)
 					.field(
-						`name, _id{"opendb-stat-app-session-result"{${type ? type + '_' : ''}active_user_count, ${type ? type + '_' : ''}new_user_count${!type ? ',total_users' : ''}}}`
-						)
-					.orderBy('start_time', 'asc')
-					.get({
-						getCount: true
-					})
+						`active_user_count,new_user_count,total_users,platform_id`
+					)
+					.groupBy(`platform_id`)
+					.groupField(
+						`sum(active_user_count) as ${type}_active_user_count, sum(new_user_count) as ${type}_new_user_count, max(total_users) as ${type}_total_users`
+					)
+					.get()
 					.then(res => {
-						const {
-							count,
-							data
-						} = res.result
-						console.log('.......chart:', data);
-						this.chartData = options
-
-						for (const item of data) {
-							const lines = item._id["opendb-stat-app-session-result"]
-							if (Array.isArray(lines)) {
-								delete(item._id)
-								const line = lines[0]
-								const name = item.name
-								let value = line && line.active_user_count ? line.active_user_count : 0
-								options.series[0].data.push({
-									name,
-									value
-								})
-								// mapfields(fieldsMap, line, item)
-							}
-						}
-						console.log(111111111, options);
-						this.chartData = options
-					}).catch((err) => {
-						console.error(err)
+						console.log(111111, res);
+						const data = res.result.data
+						this.initChartOption(data, 'dayChartsData')
 					})
 			},
 			getRangeCountData(query, type) {
-				const options = {
-					series: [{
-						data: []
-					}]
-				}
 				const {
 					pageCurrent
 				} = this.options
-				const db = uniCloud.database()
 				query = stringifyQuery(query)
-				const main = db.collection('opendb-stat-app-platforms').getTemp()
-
+				const db = uniCloud.database()
 				const sub = db.collection('opendb-stat-app-session-result')
 					.where(query)
 					.field(
-						`active_user_count, platform_id, ${type}(add(new Date(0),start_time), "Asia/Shanghai") as ${type},year(add(new Date(0),start_time), "Asia/Shanghai") as year`
+						`active_user_count, new_user_count, platform_id, ${type}(add(new Date(0),start_time), "Asia/Shanghai") as ${type},year(add(new Date(0),start_time), "Asia/Shanghai") as year`
 					)
-					.groupBy(`year, ${type}, platform_id`)
-					.groupField(`sum(active_user_count) as ${type}_active_user_count`)
+					.groupBy(`year, ${type ? type + ',' : ''}platform_id`)
+					.groupField(
+						`sum(active_user_count) as ${type}_active_user_count, sum(new_user_count) as ${type}_new_user_count`
+					)
 					.orderBy(`year asc, ${type} asc`)
-					.getTemp()
-
-				db.collection(main, sub)
-					.field(
-						`name, _id{"opendb-stat-app-session-result"{${type ? type + '_' : ''}active_user_count, ${type ? type + '_' : ''}new_user_count${type ? ',total_users' : ''}}}`
-						)
-					.orderBy('start_time', 'asc')
-					.get({
-						getCount: true
-					})
+					.get()
 					.then(res => {
-						const {
-							count,
-							data
-						} = res.result
-						console.log('.......chart:', data);
-						this.chartData = options
-
-						for (const item of data) {
-							const lines = item._id["opendb-stat-app-session-result"]
-							if (Array.isArray(lines)) {
-								delete(item._id)
-								const line = lines[0]
-								const name = item.name
-								let value = line && line.active_user_count ? line.active_user_count : 0
-								options.series[0].data.push({
-									name,
-									value
-								})
-								// mapfields(fieldsMap, line, item)
-							}
-						}
-						console.log(111111111, options);
-						this.chartData = options
-					}).catch((err) => {
-						console.error(err)
+						console.log(2222222, res);
+						const data = res.result.data
+						this.initChartOption(data, 'monChartsData', 'month')
 					})
+
 			},
 
-
-
-
-
-			jointTable(main, sub, type='') {
-				const options = {
-					series: [{
-						data: []
-					}]
-				}
+			initChartOption(data, goal, type = 'day') {
 				const db = uniCloud.database()
-				db.collection(main, sub)
-					.field(
-						`name, _id{"opendb-stat-app-session-result"{${type ? type + '_' : ''}active_user_count, ${type ? type + '_' : ''}new_user_count ${type ? ',total_users' : ''}}}`
-						)
-					.orderBy('start_time', 'asc')
-					.get({
-						getCount: true
-					})
-					.then(res => {
-						const {
-							count,
-							data
-						} = res.result
-						console.log('.......chart:', data);
-						this.chartData = options
+				db.collection('opendb-stat-app-platforms').get().then(res => {
+					const options = [{
+						field: `${type}_new_user_count`,
+						title: `${type === 'day' ? '日' : '月'}新增用户对比`,
+						series: [{
+							data: []
+						}]
+					}, {
+						field: `${type}_active_user_count`,
+						title: `${type === 'day' ? '日' : '月'}活跃用户对比`,
+						series: [{
+							data: []
+						}]
+					}]
 
+					if (type === 'day') {
+						options.unshift({
+							field: `day_total_users`,
+							title: `总用户数对比`,
+							series: [{
+								data: [],
+							}]
+						})
+					}
+
+					this[goal] = options
+					const platformsData = res.result.data
+					const platforms = {}
+					platformsData.forEach(p => {
+						platforms[p._id] = p.name
+					})
+
+					for (const chart of this[goal]) {
+						const pie = chart.series[0].data
+						const p = JSON.parse(JSON.stringify(platforms))
 						for (const item of data) {
-							const lines = item._id["opendb-stat-app-session-result"]
-							if (Array.isArray(lines)) {
-								delete(item._id)
-								const line = lines[0]
-								const name = item.name
-								let value = line && line.active_user_count ? line.active_user_count : 0
-								options.series[0].data.push({
-									name,
-									value
-								})
-								// mapfields(fieldsMap, line, item)
+							for (const key in item) {
+								if (chart.field === key) {
+									const id = item.platform_id
+									const slice = {
+										name: p[id],
+										value: item[key]
+									}
+									pie.push(slice)
+									delete p[id]
+								}
 							}
 						}
-						console.log(111111111, options);
-						this.chartData = options
-					}).catch((err) => {
-						console.error(err)
-					})
+						for (const key in p) {
+							const slice = {
+								name: p[key],
+								value: 0
+							}
+							pie.push(slice)
+						}
+					}
+				})
 			}
 
 		}
