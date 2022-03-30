1392f07c4a521f76dead66be670a0ff24750a0f6
tianjiaxing <tianjiaxing2016@163.com>
2022-03-30 16:17:51 +0800

    feat(component): uni-breadcrumb

---
+++ uni_modules/uni-breadcrumb/components/uni-breadcrumb-item/uni-breadcrumb-item.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="uni-breadcrumb-item">
+		<view :class="{
+			'uni-breadcrumb-item--slot': true,
+			'uni-breadcrumb-item--slot-link': to
+			}" @click="navTo">
+			<slot />
+		</view>
+		<i v-if="separatorClass" class="uni-breadcrumb-item--separator" :class="separatorClass" />
+		<text v-else class="uni-breadcrumb-item--separator">{{ separator }}</text>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		props: {
+			to: {
+				type: [String, Object],
+				default: ''
+			}
+		},
+		inject: ['uniBreadcrumb'],
+		computed: {
+			separator() {
+				return this.uniBreadcrumb.separator
+			},
+			separatorClass() {
+				return this.uniBreadcrumb.separatorClass
+			}
+		},
+		methods: {
+			navTo() {
+				const {
+					to,
+					$router
+				} = this
+				if (to && $router) {
+					this.replace ?
+						$router.replace(to) :
+						$router.push(to)
+
+				}
+			}
+		}
+	}
+</script>
+<style lang="scss">
+	.uni-breadcrumb-item {
+		display: flex;
+		align-items: center;
+
+		&--slot {
+			font-size: 14px;
+			color: #666;
+			padding: 0 10px;
+
+			&-link {
+				color: #333;
+				font-weight: bold;
+				/* #ifndef APP-NVUE */
+				cursor: pointer;
+				/* #endif */
+
+				&:hover {
+					color: #2979ff;
+				}
+			}
+		}
+
+		&--separator {
+			font-size: 12px;
+			color: #999;
+		}
+
+		&:last-child &--separator {
+			display: none;
+		}
+	}
+</style>
---
+++ uni_modules/uni-breadcrumb/components/uni-breadcrumb/uni-breadcrumb.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="uni-breadcrumb">
+		<slot />
+	</view>
+</template>
+<script>
+	export default {
+		props: {
+			separator: {
+				type: String,
+				default: '/'
+			},
+			separatorClass: {
+				type: String,
+				default: ''
+			}
+		},
+
+		provide() {
+			return {
+				uniBreadcrumb: this
+			}
+		}
+
+	}
+</script>
+<style lang="scss">
+	.uni-breadcrumb {
+		display: flex;
+	}
+</style>
