6cb79db69ad936262c8b90c0ff3e635b9f2f9f92
tianjiaxing <tianjiaxing2016@163.com>
2022-04-01 11:35:09 +0800

    feat: build-in uni-stat cloud functions

---
+++ uniCloud-aliyun/cloudfunctions/uni-stat-cron/index.js
@@ -0,0 +1,7 @@
+'use strict';
+const uniStat = require('uni-stat')
+exports.main = async (event, context) => {
+	//数据跑批处理函数
+	let res = await uniStat.init().statCron(context)
+	return res
+};
---
+++ uniCloud-aliyun/cloudfunctions/uni-stat-report/index.js
@@ -0,0 +1,7 @@
+'use strict';
+const uniStat = require('uni-stat')
+exports.main = async (event, context) => {
+	//数据上报处理函数
+	let res = await uniStat.init().report(event)
+	return res
+};
