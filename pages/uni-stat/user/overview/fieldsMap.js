b3578201cbd9e5e217cdf6957219ec86560ca795
tianjiaxing <tianjiaxing2016@163.com>
2022-04-20 21:04:09 +0800

    chore(user): remove needless fields

--- pages/uni-stat/user/overview/fieldsMap.js
+++ pages/uni-stat/user/overview/fieldsMap.js
@@ -14,18 +14,6 @@ const fieldsMap = [{
 	value: 0,
 	contrast: 0
 }, {
-	title: '访问次数',
-	field: 'page_visit_count',
-	tooltip: '访问过应用内任意页面总次数，多个页面之间跳转、同一页面的重复访问计为多次访问',
-	value: 0,
-	contrast: 0
-}, {
-	title: '启动次数',
-	field: 'app_launch_count',
-	tooltip: '用户从打开应用到主动关闭应用或超时退出计为一次启动',
-	value: 0,
-	contrast: 0
-}, {
 	title: '次均停留时长',
 	field: 'avg_session_time',
 	formatter: ':',
@@ -41,18 +29,6 @@ const fieldsMap = [{
 	value: 0,
 	contrast: 0,
 	stat: 'avg'
-}, {
-	title: '跳出率',
-	field: 'bounceRate',
-	computed: 'bounce_times/app_launch_count',
-	formatter: '%',
-	tooltip: '只浏览一个页面便离开应用的次数占总启动次数的百分比',
-	value: 0,
-	contrast: 0,
-	fix: 2
-}, {
-	field: 'bounce_times',
-	disable: true
 }, {
 	title: '总用户数',
 	field: 'total_users',
--- pages/uni-stat/user/trend/fieldsMap.js
+++ pages/uni-stat/user/trend/fieldsMap.js
@@ -15,16 +15,6 @@ export default [{
 	tooltip: '访问过应用内任意页面的总用户数（去重）',
 	value: 0
 }, {
-	title: '访问次数',
-	field: 'page_visit_count',
-	tooltip: '访问过应用内任意页面总次数，多个页面之间跳转、同一页面的重复访问计为多次访问',
-	value: 0
-}, {
-	title: '启动次数',
-	field: 'app_launch_count',
-	tooltip: '用户从打开应用到主动关闭应用或超时退出计为一次启动',
-	value: 0
-}, {
 	title: '次均停留时长',
 	field: 'avg_session_time',
 	formatter: ':',
@@ -38,18 +28,6 @@ export default [{
 	tooltip: '平均每个用户停留在应用内的总时长，即应用停留总时长/活跃用户',
 	value: 0,
 	stat: 'avg'
-}, {
-	title: '跳出率',
-	field: 'bounceRate',
-	computed: 'bounce_times/app_launch_count',
-	formatter: '%',
-	tooltip: '只浏览一个页面便离开应用的次数占总启动次数的百分比',
-	value: 0,
-	contrast: 0,
-	fix: 2
-}, {
-	field: 'bounce_times',
-	disable: true
 }, {
 	title: '总用户数',
 	field: 'total_users',
