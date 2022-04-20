73b7127052c4a71998bceab1ed20658b0e9040a8
tianjiaxing <tianjiaxing2016@163.com>
2022-04-20 14:31:35 +0800

    chore: change project files structure

---
+++ pages/uni-stat/user/stickiness/stickiness.vue
@@ -0,0 +1,364 @@
+<template>
+	<view class="fix-top-window">
+		<view class="uni-header">
+			<uni-stat-breadcrumb class="uni-stat-breadcrumb-on-phone" />
+			<view class="uni-group">
+				<!-- <view class="uni-title">用户忠诚度</view> -->
+				<view class="uni-sub-title hide-on-phone">用户忠诚度用户对您应用的访问深度及访问频次情况。助您了解用户对应用的粘度，尤其在对内容改进后，效果是否有所提升</view>
+			</view>
+		</view>
+		<view class="uni-container">
+			<view class="uni-stat--x flex">
+				<uni-stat-select mode="app" label="应用选择" v-model="query.appid" :clear="false" />
+			</view>
+			<view class="uni-stat--x">
+				<uni-stat-tabs label="平台选择" type="boldLine" mode="platform" v-model="query.platform_id"
+					@change="changePlatform" />
+				<uni-stat-select mode="channel" label="渠道选择" :query="channelQuery" v-model="query.channel_id" />
+			</view>
+			<view class="uni-stat--x flex">
+				<uni-stat-tabs label="日期选择" :current="currentDateTab" mode="date" @change="changeTimeRange" />
+				<uni-datetime-picker type="daterange" :end="new Date().getTime()"  v-model="query.start_time" returnType="timestamp"
+					:clearIcon="false" class="uni-stat-datetime-picker"
+					:class="{'uni-stat__actived': currentDateTab < 0 && !!query.start_time.length}"
+					@change="useDatetimePicker" />
+			</view>
+			<view class="uni-stat--x mb-l" style="padding-top: 0;">
+				<view class="mb-m line-bottom">
+					<uni-stat-tabs type="boldLine" :tabs="types" v-model="type"
+						style="line-height: 40px; margin-bottom: -17px;" />
+				</view>
+				<uni-stat-panel v-if="type === 'visit_depth_data'" :items="panelData" class="uni-stat-panel" />
+				<uni-stat-tabs type="box" :tabs="fields" v-model="field" class="mb-l" />
+				<view class="p-m">
+					<view class="uni-charts-box">
+						<qiun-data-charts type="column" :chartData="chartData" echartsH5 echartsApp />
+					</view>
+				</view>
+			</view>
+			<view class="uni-stat--x p-m">
+				<uni-stat-table :data="tableData" :filedsMap="fieldsMap" :loading="loading" />
+			</view>
+		</view>
+
+		<!-- #ifndef H5 -->
+		<fix-window />
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	import {
+		mapfields,
+		stringifyQuery,
+		getTimeOfSomeDayAgo,
+		division,
+		format
+	} from '@/js_sdk/uni-stat/util.js'
+	import fieldsMap from './fieldsMap.js'
+	export default {
+		data() {
+			return {
+				fieldsMap,
+				query: {
+					// dimension: "hour",
+					appid: '',
+					platform_id: '',
+					channel_id: '',
+					start_time: [],
+				},
+				loading: false,
+				currentDateTab: 0,
+				// currentChartTab: ,
+				tableData: [],
+				panelData: fieldsMap.filter(f => f.hasOwnProperty('value')),
+				chartData: {},
+				type: 'visit_depth_data',
+				types: [{
+					_id: 'visit_depth_data',
+					name: '访问深度'
+				}, {
+					_id: 'duration_data',
+					name: '访问时长'
+				}],
+				field: 'visit_users',
+				fields: [{
+					_id: 'visit_users',
+					name: '访问人数'
+				}, {
+					_id: 'visit_times',
+					name: '访问次数'
+				}],
+				options: {
+					visit_depth_data: {
+						prefix: 'p',
+						title: '页',
+						value: [1, 2, 3, 4, 5, 10]
+					},
+					duration_data: {
+						prefix: 's',
+						title: '秒',
+						value: [0, 3, 6, 11, 21, 31, 51, 100]
+					}
+				}
+			}
+		},
+		computed: {
+			fieldName() {
+				return this.fields.forEach(item => {
+					if (item._id === this.field) {
+						return item.name
+					}
+				})
+			},
+			channelQuery() {
+				const platform_id = this.query.platform_id
+				return stringifyQuery({
+					platform_id
+				})
+			}
+		},
+		watch: {
+			query: {
+				deep: true,
+				handler(val) {
+					this.getAllData(val)
+				}
+			},
+			type() {
+				this.getAllData(this.query)
+			},
+			field() {
+				this.getAllData(this.query)
+			}
+		},
+		methods: {
+			useDatetimePicker() {
+				this.currentDateTab = -1
+			},
+			changePlatform() {
+				this.query.channel_id = ''
+			},
+			changeTimeRange(id, index) {
+				this.currentDateTab = index
+				const start = getTimeOfSomeDayAgo(id),
+					end = getTimeOfSomeDayAgo(0) - 1
+				this.query.start_time = [start, end]
+			},
+
+			createStr(fields, type = "visit_depth_data") {
+				const l = fields.length
+				const p = this.options[type].prefix
+				const value = this.options[type].value
+				const strArr = value.map(item => {
+					return fields.map(field => {
+						return `sum(${type}.${field}.${p + '_' + item}) as ${l > 1 ? field + '_' + p +'_'+item :  p + '_' + item}`
+					})
+				})
+				const str = strArr.join()
+				return str
+			},
+
+			parseChars(str) {
+				str = str.split('_')
+				const option = this.options[this.type]
+				let chars = option.title
+				const strArr = option.value.forEach((val, i) => {
+					const next = option.value[i + 1]
+					if (val === Number(str[str.length - 1])) {
+						if (!next) {
+							chars = val + '+' + chars
+						} else if (val + 1 === next) {
+							chars = val + chars
+						} else {
+							chars = val + '-' + (next - 1) + chars
+						}
+					}
+				})
+				return chars
+			},
+
+			getAllData(query) {
+				this.getPanelData(query)
+				this.getChartData(query, this.field, this.fieldName)
+				this.getTabelData(query)
+			},
+
+			getChartData(query, field = this.field, name = this.fields.find(f => f._id === this.field).name) {
+				this.chartData = {}
+				query = stringifyQuery(query)
+				const groupField = this.createStr([field], this.type)
+				const db = uniCloud.database()
+				db.collection('opendb-stat-loyalty-result')
+					.where(query)
+					.groupBy('appid')
+					.groupField(groupField)
+					.orderBy('start_time', 'asc')
+					.get({
+						getCount: true
+					})
+					.then(res => {
+						let {
+							count,
+							data
+						} = res.result
+						data = data[0]
+						const options = {
+							categories: [],
+							series: [{
+								name,
+								data: []
+							}]
+						}
+						for (const key in data) {
+							if (key !== 'appid') {
+								const x = this.parseChars(key)
+								const y = data[key]
+								if (y) {
+									options.series[0].data.push(y)
+									options.categories.push(x)
+								}
+							}
+						}
+						this.chartData = options
+					}).catch((err) => {
+						console.error(err)
+						// err.message 错误信息
+						// err.code 错误码
+					}).finally(() => {
+						this.loading = false
+					})
+			},
+
+			getTabelData(query) {
+				query = stringifyQuery(query)
+				const groupField = this.createStr(['visit_users', 'visit_times'], this.type)
+				this.loading = true
+				const db = uniCloud.database()
+				db.collection('opendb-stat-loyalty-result')
+					.where(query)
+					.groupBy('appid')
+					.groupField(groupField)
+					.orderBy('start_time', 'asc')
+					.get({
+						getCount: true
+					})
+					.then(res => {
+						const {
+							count,
+							data
+						} = res.result
+						const type = this.type
+						const rows = []
+						let splitor = this.options[type].prefix
+						splitor = `_${splitor}_`
+						for (const item of data) {
+							for (const key in item) {
+								if (key !== 'appid') {
+									const row = {}
+									const keys = key.split(splitor)
+									row.name = keys[1]
+									row[keys[0]] = item[key]
+									rows.push(row)
+								}
+							}
+						}
+						const tableData = []
+						const reducer = (previousValue, currentValue) => previousValue + currentValue;
+						const total = {}
+
+						let users = rows.filter(row => row.visit_users)
+							.map(row => row.visit_users)
+						users = users.length ? users.reduce(reducer) : 0
+						let times = rows.filter(row => row.visit_times)
+							.map(row => row.visit_times)
+						times = times.length ? times.reduce(reducer) : 0
+						total.visit_times = times
+						total.visit_users = users
+						this.options[type].value.forEach(val => {
+							const item = {}
+							item.name = val + 'p'
+							rows.forEach(row => {
+								if (Number(row.name) === val) {
+									for (const key in row) {
+										if (key !== name) {
+											item[key] = row[key]
+											item['total_' + key] = total[key]
+										}
+									}
+								}
+							})
+							item.name = this.parseChars(String(val))
+							tableData.push(item)
+						})
+						for (const item of tableData) {
+							mapfields(fieldsMap, item, item)
+						}
+						// this.options.total = count
+						this.tableData = []
+						this.tableData = tableData
+					}).catch((err) => {
+						console.error(err)
+						// err.message 错误信息
+						// err.code 错误码
+					}).finally(() => {
+						this.loading = false
+					})
+			},
+
+			getPanelData(query) {
+				query = stringifyQuery(query)
+				const db = uniCloud.database()
+				const subTable = db.collection('opendb-stat-result')
+					.where(query)
+					.groupBy('appid')
+					.groupField(
+						'sum(page_visit_count) as total_visit_times, max(total_users) as total_visit_users'
+					)
+					.orderBy('start_time', 'desc')
+					.get({
+						getCount: true
+					})
+					.then(res => {
+						const items = res.result.data[0]
+						this.panelData = []
+						this.panelData = mapfields(fieldsMap, items, undefined, 'total_')
+					})
+			},
+
+			navTo(id) {
+				const url = `/pages/uni-stat/overview/overview?id=${id}`
+				uni.navigateTo({
+					url
+				})
+			}
+		}
+
+	}
+</script>
+
+<style lang="scss">
+	.flex {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+	}
+
+	.label-text {
+		font-size: 14px;
+		color: #666;
+		margin: auto 0;
+		margin-right: 5px;
+	}
+
+	.line-bottom {
+		border-bottom: 2px solid #eee;
+	}
+
+	.uni-stat-panel {
+		box-shadow: unset;
+		border-bottom: 1px solid #eee;
+		padding: 0;
+		margin: 0 15px;
+	}
+</style>
---
+++ pages/uni-stat/user/trend/trend.vue
@@ -0,0 +1,359 @@
+<template>
+	<view class="fix-top-window">
+		<view class="uni-header">
+			<uni-stat-breadcrumb class="uni-stat-breadcrumb-on-phone" />
+			<view class="uni-group">
+				<!-- <view class="uni-title">趋势分析</view> -->
+				<view class="uni-sub-title hide-on-phone">各指标趋势分析</view>
+			</view>
+		</view>
+		<view class="uni-container">
+			<view class="uni-stat--x flex">
+				<uni-stat-select mode="app" label="应用选择" v-model="query.appid" :clear="false" />
+			</view>
+			<view class="uni-stat--x">
+				<uni-stat-tabs label="平台选择" type="boldLine" mode="platform" v-model="query.platform_id"
+					@change="changePlatform" />
+				<uni-stat-select mode="channel" label="渠道选择" :query="channelQuery" v-model="query.channel_id" />
+			</view>
+			<view class="uni-stat--x flex">
+				<uni-stat-tabs label="日期选择" :current="currentDateTab" mode="date" @change="changeTimeRange" />
+				<uni-datetime-picker type="daterange" :end="new Date().getTime()" v-model="query.start_time"
+					returnType="timestamp" :clearIcon="false" class="uni-stat-datetime-picker"
+					:class="{'uni-stat__actived': currentDateTab < 0 && !!query.start_time.length}"
+					@change="useDatetimePicker" />
+				<uni-stat-tabs type="box" :current="currentDimensionTab" :tabs="dimensionTabs" class="ml-l"
+					v-model="query.dimension" />
+			</view>
+			<uni-stat-panel :items="panelData" />
+
+			<view class="uni-stat--x p-m">
+				<view class="label-text mb-l">
+					趋势图
+				</view>
+				<uni-stat-tabs type="box" v-model="chartTab" :tabs="chartTabs" class="mb-l" @change="changeChartTab" />
+				<view class="uni-charts-box">
+					<qiun-data-charts type="area" :chartData="chartData" echartsH5 echartsApp />
+				</view>
+			</view>
+
+			<view class="uni-stat--x p-m">
+				<uni-stat-table :data="tableData" :filedsMap="fieldsMap" :loading="loading" />
+				<view class="uni-pagination-box">
+					<picker class="select-picker" mode="selector" :value="options.pageSizeIndex"
+						:range="options.pageSizeRange" @change="changePageSize">
+						<button type="default" size="mini" :plain="true">
+							<text>{{pageSize}} 条/页</text>
+							<uni-icons class="select-picker-icon" type="arrowdown" size="12" color="#999"></uni-icons>
+						</button>
+					</picker>
+					<uni-pagination show-icon :page-size="pageSize" :current="options.pageCurrent"
+						:total="options.total" @change="changePageCurrent" />
+				</view>
+			</view>
+		</view>
+
+		<!-- #ifndef H5 -->
+		<fix-window />
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	import {
+		mapfields,
+		stringifyQuery,
+		stringifyField,
+		stringifyGroupField,
+		getTimeOfSomeDayAgo,
+		division,
+		format,
+		formatDate,
+		getCurrentTotalUser,
+		debounce
+	} from '@/js_sdk/uni-stat/util.js'
+	import fieldsMap from './fieldsMap.js'
+	export default {
+		data() {
+			return {
+				fieldsMap,
+				query: {
+					dimension: "hour",
+					appid: '',
+					platform_id: '',
+					channel_id: '',
+					start_time: [],
+				},
+				options: {
+					pageCurrent: 1, // 当前页
+					total: 0, // 数据总量
+					pageSizeIndex: 0, // 与 pageSizeRange 一起计算得出 pageSize
+					pageSizeRange: [10, 20, 50, 100],
+				},
+				loading: false,
+				currentDateTab: 0,
+				currentDimensionTab: 0,
+				tableData: [],
+				panelData: fieldsMap.filter(f => f.hasOwnProperty('value')),
+				chartData: {},
+				chartTab: 'new_user_count'
+			}
+		},
+		created() {
+			this.debounceGetAllData = debounce(() => this.getAllData(this.query))
+		},
+		computed: {
+			pageSize() {
+				const {
+					pageSizeRange,
+					pageSizeIndex
+				} = this.options
+				return pageSizeRange[pageSizeIndex]
+			},
+			chartTabs() {
+				const tabs = []
+				fieldsMap.forEach(item => {
+					const {
+						field: _id,
+						title: name
+					} = item
+					const isTab = item.hasOwnProperty('value')
+					if (_id && name && isTab) {
+						tabs.push({
+							_id,
+							name
+						})
+					}
+				})
+				return tabs
+			},
+			dimensionTabs() {
+				const tabs = [{
+					_id: 'hour',
+					name: '按时'
+				}, {
+					_id: 'day',
+					name: '按日'
+				}, {
+					_id: 'week',
+					name: '按周'
+				}, {
+					_id: 'month',
+					name: '按月'
+				}]
+				if (!this.getDays()) {
+					this.query.dimension = 'hour'
+					tabs.forEach((tab, index) => {
+						if (tab._id === 'hour') {
+							tab.disabled = false
+						} else {
+							tab.disabled = true
+						}
+					})
+					this.currentDimensionTab = 0
+				} else {
+					this.query.dimension = 'day'
+					tabs.forEach((tab, index) => {
+						if (tab._id === 'hour') {
+							tab.disabled = true
+						} else {
+							tab.disabled = false
+						}
+					})
+					this.currentDimensionTab = 1
+				}
+				return tabs
+			},
+			channelQuery() {
+				const platform_id = this.query.platform_id
+				return stringifyQuery({
+					platform_id
+				})
+			}
+		},
+		watch: {
+			query: {
+				deep: true,
+				handler(val) {
+					this.debounceGetAllData(val)
+				}
+			}
+		},
+		methods: {
+			getDays() {
+				if (!this.query.start_time.length) return true
+				const day = 24 * 60 * 60 * 1000
+				const [start, end] = this.query.start_time
+				const lessTwoDay = end - start >= day
+				return lessTwoDay
+			},
+			useDatetimePicker() {
+				this.currentDateTab = -1
+			},
+			changePlatform() {
+				this.query.channel_id = ''
+			},
+			changeTimeRange(id, index) {
+				this.currentDateTab = index
+				const day = 24 * 60 * 60 * 1000
+				let start, end
+				start = getTimeOfSomeDayAgo(id)
+				if (!id) {
+					end = getTimeOfSomeDayAgo(0) + day - 1
+				} else {
+					end = getTimeOfSomeDayAgo(0) - 1
+				}
+				this.query.start_time = [start, end]
+			},
+			changePageCurrent(e) {
+				this.options.pageCurrent = e.current
+				this.getTabelData(this.query)
+			},
+
+			changePageSize(e) {
+				const {
+					value
+				} = e.detail
+				// this.options.pageCurrent = 1 // 重置分页
+				this.options.pageSizeIndex = value
+				this.getTabelData(this.query)
+			},
+
+			changeChartTab(id, index, name) {
+				this.getChartData(this.query, id, name)
+			},
+
+			getAllData(query) {
+				this.getPanelData()
+				this.getChartData(query)
+				this.getTabelData(query)
+			},
+
+			getChartData(query, field = this.chartTab, name = '新增用户') {
+				this.chartData = {}
+				query = stringifyQuery(query, true)
+				const dimension = this.query.dimension
+				const db = uniCloud.database()
+				db.collection('opendb-stat-result')
+					.where(query)
+					.field(`${stringifyField(fieldsMap, field)}, start_time`)
+					.groupBy('start_time')
+					.groupField(stringifyGroupField(fieldsMap, field))
+					.orderBy('start_time', 'asc')
+					.get({
+						getCount: true
+					})
+					.then(res => {
+						const {
+							count,
+							data
+						} = res.result
+						const options = {
+							categories: [],
+							series: [{
+								name,
+								data: []
+							}]
+						}
+						let mapper = fieldsMap.filter(f => f.field === field)
+						mapper = JSON.parse(JSON.stringify(mapper))
+						delete mapper[0].value
+						mapper[0].formatter = ''
+						for (const item of data) {
+							mapfields(mapper, item, item)
+							const x = formatDate(item.start_time, dimension)
+							let y = item[field]
+							if (y) {
+								options.series[0].data.push(y)
+								options.categories.push(x)
+							}
+						}
+						this.chartData = options
+					}).catch((err) => {
+						console.error(err)
+						// err.message 错误信息
+						// err.code 错误码
+					}).finally(() => {
+						this.loading = false
+					})
+			},
+
+			getTabelData(query) {
+				const {
+					pageCurrent
+				} = this.options
+				query = stringifyQuery(query, true)
+				this.options.pageCurrent = 1 // 重置分页
+				this.loading = true
+				const db = uniCloud.database()
+				db.collection('opendb-stat-result')
+					.where(query)
+					.field(stringifyField(fieldsMap))
+					.groupBy('start_time')
+					.groupField(stringifyGroupField(fieldsMap))
+					.orderBy('start_time', 'desc')
+					.skip((pageCurrent - 1) * this.pageSize)
+					.limit(this.pageSize)
+					.get({
+						getCount: true
+					})
+					.then(res => {
+						const {
+							count,
+							data
+						} = res.result
+						for (const item of data) {
+							let date = item.start_time
+							if (date) {
+								const dimension = this.query.dimension
+								item.start_time = formatDate(date, dimension)
+							}
+							mapfields(fieldsMap, item, item)
+						}
+						this.tableData = []
+						this.options.total = count
+						this.tableData = data
+					}).catch((err) => {
+						console.error(err)
+						// err.message 错误信息
+						// err.code 错误码
+					}).finally(() => {
+						this.loading = false
+					})
+			},
+
+			getPanelData() {
+				let cloneQuery = JSON.parse(JSON.stringify(this.query))
+				cloneQuery.dimension = 'day'
+				let query = stringifyQuery(cloneQuery)
+				const db = uniCloud.database()
+				const subTable = db.collection('opendb-stat-result')
+					.where(query)
+					.field(`${stringifyField(fieldsMap)},stat_date`)
+					.groupBy('appid')
+					.groupField(stringifyGroupField(fieldsMap))
+					.orderBy('stat_date', 'desc')
+					.get()
+					.then(res => {
+						const item = res.result.data[0]
+						item && (item.total_users = 0)
+						getCurrentTotalUser.call(this, cloneQuery)
+						this.panelData = []
+						this.panelData = mapfields(fieldsMap, item)
+					})
+			},
+
+			navTo(id) {
+				const url = `/pages/uni-stat/overview/overview?id=${id}`
+				uni.navigateTo({
+					url
+				})
+			}
+		}
+
+	}
+</script>
+
+<style>
+
+</style>
