5d574e2d5083936d9f1afc250cfee5d59123489a
DCloud_LXH <283700113@qq.com>
2022-07-07 17:40:58 +0800

    chore: uni-portal.vue

--- pages/system/app/uni-portal/uni-portal.vue
+++ pages/system/app/uni-portal/uni-portal.vue
@@ -5,31 +5,27 @@
 		<h3 class="text-separated" style="padding: 0 0 20rpx 0;">步骤1：了解“统一发布页”</h3>
 
 		<view style="margin-top: 20rpx;">
-
-			<view class="">
+			<view class="text-separated">
 				<text class="strong">uni-portal </text>
-				<text class="text-separated">是 uni-app 提供的一套开箱即用的“统一发布页”。</text>
+				<text>是 uni-app 提供的一套开箱即用的“统一发布页”。</text>
 			</view>
-			<view class="">
+			<view class="text-separated">
 				<text class="strong">uni-portal </text>
-				<text class="text-separated">可作为面向用户的统一业务名片，在一个页面集中展现：App下载地址、小程序二维码、H5访问链接等信息。</text>
+				<text>可作为面向用户的统一业务名片，在一个页面集中展现：App下载地址、小程序二维码、H5访问链接等信息。</text>
 			</view>
 
-			<view class="">
-
-				<!-- #ifdef H5 -->
-				<text class="text-separated" style="font-size: 16px;">uni-app 官方示例的发布页就是基于<text class="strong">uni-portal </text> 制作的，<a href="https://hellouniapp.dcloud.net.cn/portal">点击体验</a>
-
+			<!-- #ifdef H5 -->
+			<view class="text-separated">
+				<text style="font-size: 16px;">uni-app 官方示例的发布页就是基于<text class="strong">uni-portal </text> 制作的，<a
+						href="https://hellouniapp.dcloud.net.cn/portal" target="_blank" class="a-label">点击体验</a>
 				</text>
-				<!-- #endif -->
-
 			</view>
-
+			<!-- #endif -->
 		</view>
 
 		<h3 class="text-separated" style="padding: 40rpx 0 20rpx 0;">步骤2：获取“统一发布页”</h3>
-		<view class="flex" style="margin-top: 20rpx;">
-			<text style="font-size: 14px;">
+		<view class="flex text-separated" style="margin-top: 20rpx;">
+			<text>
 				<view class="strong">uni-portal </view> 可根据「应用管理」中所填写的应用信息，一键生成发布页：
 			</text>
 			<button class="custom-button" size="mini" type="primary" @click="publish"
@@ -38,22 +34,20 @@
 
 		<h3 class="text-separated" style="padding: 40rpx 0 20rpx 0;">步骤3：上传“统一发布页”</h3>
 
-		<view style="margin-top: 20px;">
-
-			<view class="">
-				<text class="text-separated">
+		<view style="margin-top: 20rpx;">
+			<view class="text-separated">
+				<text>
 					步骤2下载的“统一发布页”，是一个静态HTML页面，你可以直接在本地浏览器中打开访问。
 				</text>
 			</view>
 
-			<view class="">
-				<text class="text-separated">
-					为了让用户访问到这个“统一发布页”，你需要将该静态HTML文件上传到你的服务器中；推荐使用<a href="https://uniapp.dcloud.io/uniCloud/hosting" target="_blank" class="a-label" style="padding: 5px;">前端网页托管</a>，因为前端网页托管具备使用更简单、价格更便宜、访问更快等优点。
+			<view class="text-separated">
+				<text>
+					为了让用户访问到这个“统一发布页”，你需要将该静态HTML文件上传到你的服务器中；推荐使用<a href="https://uniapp.dcloud.io/uniCloud/hosting"
+						target="_blank" class="a-label" style="padding: 5px;">前端网页托管</a>，因为前端网页托管具备使用更简单、价格更便宜、访问更快等优点。
 				</text>
 			</view>
-
 		</view>
-
 	</view>
 </template>
 
