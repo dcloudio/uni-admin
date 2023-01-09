3932a393891ed5ec268828fd0f1acb32e6d411f0
VK <370725567@qq.com>
2023-01-09 20:32:17 +0800

    修复 用户管理-编辑时，若用户拥有的应用未添加到应用管理时，点击保存会导致用户丢失该应用的appid，进而导致下次登录提示未在该应用注册的问题。

--- pages/system/user/edit.vue
+++ pages/system/user/edit.vue
@@ -24,10 +24,12 @@
 				<span class="link-btn" @click="gotoTagAdd">新增</span>
 				<span class="link-btn" @click="gotoTagList" style="margin-left: 10px;">管理</span>
 			</uni-forms-item>
-			<uni-forms-item name="authorizedApp" label="可登录应用" class="flex-center-x">
-				<uni-data-checkbox :multiple="true" v-model="formData.authorizedApp" collection="opendb-app-list"
-					field="appid as value, name as text"></uni-data-checkbox>
-				<span class="link-btn" @click="gotoAppList">管理</span>
+			<uni-forms-item name="authorizedApp" label="可登录应用">
+				<view class="uni-forms-item-flex-center-x">
+					<uni-data-checkbox :multiple="true" v-model="formData.authorizedApp" :localdata="appList"></uni-data-checkbox>
+					<span class="link-btn" @click="gotoAppList">管理</span>
+				</view>
+				<view v-if="formDataId === userId" class="uni-form-item-tips">当前有未添加的应用{{ unknownAppidsCom }}，建议点击右侧管理进行添加</view>
 			</uni-forms-item>
 			<uni-forms-item name="mobile" label="手机号">
 				<uni-easyinput v-model="formData.mobile" :clearable="false" placeholder="请输入手机号" />
@@ -36,8 +38,9 @@
 				<uni-easyinput v-model="formData.email" :clearable="false" placeholder="请输入邮箱" />
 			</uni-forms-item>
 			<uni-forms-item name="status" label="用户状态">
-				<switch v-if="Number(formData.status) < 2" @change="binddata('status', $event.detail.value)" :checked="formData.status" />
+				<switch v-if="Number(formData.status) < 2" @change="binddata('status', $event.detail.value)" :checked="formData.status" :disabled="formDataId === userId" />
 				<view v-else class="uni-form-item-empty">{{parseUserStatus(formData.status)}}</view>
+				<view v-if="formDataId === userId" class="uni-form-item-tips">请勿禁用当前登录的账号</view>
 			</uni-forms-item>
 			<view class="uni-button-group">
 				<button style="width: 100px;" type="primary" class="uni-button" @click="submitForm">{{$t('common.button.submit')}}</button>
@@ -86,14 +89,19 @@
 						}]
 					}
 				},
-				roles: []
+				roles: [],
+				userId:"",
+				appList:[],
+				unknownAppids:[]
 			}
 		},
 		onLoad(e) {
 			const id = e.id
 			this.formDataId = id
+			let userInfo = uni.getStorageSync("uni-id-pages-userInfo") || {};
+			this.userId = userInfo._id;
 			this.getDetail(id)
-			this.loadroles()
+			this.loadroles();
 		},
 		methods: {
 			/**
@@ -206,7 +214,8 @@
 							if (data.status === 1) {
 								data.status = false
 							}
-							this.formData = Object.assign(this.formData, data)
+							this.formData = Object.assign(this.formData, data);
+							this.loadAppList(this.formData.authorizedApp);
 						}
 					}).catch((err) => {
 						uni.showModal({
@@ -241,6 +250,36 @@
 					})
 				})
 			},
+			loadAppList(authorizedApp){
+				db.collection('opendb-app-list').limit(500).get().then(res => {
+					let list = res.result.data.map((item, index) => {
+						return {
+							value: item.appid,
+							text: item.name
+						}
+					});
+					if (!list) list = [];
+					authorizedApp.map((appid) => {
+						let info = list.find((item) => {
+							return item.value === appid;
+						});
+						if (!info) {
+							this.unknownAppids.push(appid);
+							list.push({
+								value: appid,
+								text: `未知应用${appid}`
+							});
+						}
+					});
+					this.appList = list;
+				}).catch(err => {
+					uni.showModal({
+						title: '提示',
+						content: err.message,
+						showCancel: false
+					})
+				})
+			},
 			// status 对应文字显示
 			parseUserStatus(status) {
 				if (status === 0) {
@@ -254,6 +293,18 @@
 				} else {
 					return '启用'
 				}
+			}
+		},
+		computed:{
+			unknownAppidsCom(){
+				let str = "";
+				this.unknownAppids.map((item, index) => {
+					str += item;
+					if (index !== this.unknownAppids.length-1){
+						str += "、";
+					}
+				});
+				return str;
 			}
 		}
 	}
@@ -275,5 +326,11 @@
 	}
 	::v-deep .uni-forms-item__label {
 		width: 90px !important;
+	}
+
+	.uni-forms-item-flex-center-x {
+		display: flex;
+		align-items: center;
+		flex-wrap: wrap;
 	}
 </style>
