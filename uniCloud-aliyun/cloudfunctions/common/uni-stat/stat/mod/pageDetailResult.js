7c073ab1d28defe0f96b8c5cd2b40c4bd9970c6d
jiarongping <jiarongping@163.com>
2024-07-04 12:24:19 +0800

    fix: uni统计文件命名调整

--- uniCloud-aliyun/cloudfunctions/common/uni-stat/stat/mod/pageDetailResult.js
+++ uniCloud-aliyun/cloudfunctions/common/uni-stat/stat/mod/pageDetailResult.js
@@ -11,7 +11,7 @@ const ShareLog = require('./shareLog')
 const {
 	DateTime
 } = require('../lib')
-module.exports = class PageResult extends BaseMod {
+module.exports = class PageDetailResult extends BaseMod {
 	constructor() {
 		super()
 		this.tableName = 'page-detail-result'
